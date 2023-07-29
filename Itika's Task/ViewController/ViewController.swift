e72304c9615cf18ad9d25fb9e3ae79dafbf519a0
Itika Soni <itikasoni@Itikas-MacBook-Air.local>
2023-07-29 14:11:12 +0530

    final

--- Itika's Task/ViewController/ViewController.swift
+++ Itika's Task/ViewController/ViewController.swift
@@ -21,9 +21,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         //getUserDetailsApi()
       //  getUserListApi()
-        getJsonApi()
+        getRowJsonApi()
     }
     
+    func getRowJsonApi () {
+        // Your API URL goes here
+        let rowData = """
+        [{"address":{"geolocation":{"lat":"-37.3159","long":"81.1496"},"city":"kilcoole","street":"new road","number":7682,"zipcode":"12926-3874"},"id":1,"email":"john@gmail.com","username":"johnd","password":"m38rmF$","name":{"firstname":"john","lastname":"doe"},"phone":"1-570-236-7033","__v":0},{"address":{"geolocation":{"lat":"-37.3159","long":"81.1496"},"city":"kilcoole","street":"Lovers Ln","number":7267,"zipcode":"12926-3874"},"id":2,"email":"morrison@gmail.com","username":"mor_2314","password":"83r5^_","name":{"firstname":"david","lastname":"morrison"},"phone":"1-570-236-7033","__v":0},{"address":{"geolocation":{"lat":"40.3467","long":"-30.1310"},"city":"Cullman","street":"Frances Ct","number":86,"zipcode":"29567-1452"},"id":3,"email":"kevin@gmail.com","username":"kevinryan","password":"kev02937@","name":{"firstname":"kevin","lastname":"ryan"},"phone":"1-567-094-1345","__v":0},{"address":{"geolocation":{"lat":"50.3467","long":"-20.1310"},"city":"San Antonio","street":"Hunters Creek Dr","number":6454,"zipcode":"98234-1734"},"id":4,"email":"don@gmail.com","username":"donero","password":"ewedon","name":{"firstname":"don","lastname":"romer"},"phone":"1-765-789-6734","__v":0},{"address":{"geolocation":{"lat":"40.3467","long":"-40.1310"},"city":"san Antonio","street":"adams St","number":245,"zipcode":"80796-1234"},"id":5,"email":"derek@gmail.com","username":"derek","password":"jklg*_56","name":{"firstname":"derek","lastname":"powell"},"phone":"1-956-001-1945","__v":0}]
+        """
+        
+        
+        guard let data = rowData.data(using: .utf8) else {
+            print("Invalid Data")
+            return
+        }
+       
+        do {
+            let userDetails = try JSONDecoder().decode(UserList.self, from: data)
+            print(userDetails);
+            DispatchQueue.main.async {
+                self.countryCode.text = userDetails[0].address?.city
+            }
+            
+        } catch {
+            print("Error parsing JSON: \(error.localizedDescription)")
+        }
+
+    }
     
     func getUserListApi () {
         // Your API URL goes here
